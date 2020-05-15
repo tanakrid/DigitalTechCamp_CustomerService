605a6b67c359d30b446b9ba4742f0567e72ea82e
tanakrid chanburi <tanakrid.c@ku.th>
2020-05-15 07:19:12 +0700

    add some route

--- src/main/java/com/digitalacademy/controller/CustomerController.java
+++ src/main/java/com/digitalacademy/controller/CustomerController.java
@@ -3,12 +3,16 @@ package com.digitalacademy.controller;
 import com.digitalacademy.model.Customer;
 import com.digitalacademy.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -38,5 +42,30 @@ public class CustomerController {
         }else{
             return ResponseEntity.notFound().build();
         }
+    }
+
+    @GetMapping(params = "name")
+    public ResponseEntity<?> getCustomerByName(@RequestParam(value = "name") String name){
+        List<Customer> customers = customerService.getCustomerName(name);
+        return customers != null && !customers.isEmpty() ? ResponseEntity.ok(customers) : ResponseEntity.notFound().build();
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<?> createCustomer(@Valid @RequestBody Customer body){
+        Customer customer = customerService.createCustomer(body);
+        return ResponseEntity.status(HttpStatus.CREATED).body(customer);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> putCustomer(@PathVariable Long id, @Valid @RequestBody Customer body){
+        body.setId(id);
+        Customer customer = customerService.updateCustomer(id, body);
+        return customer != null ? ResponseEntity.ok(customer)
+                : ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteCustomer(@PathVariable Long id){
+        return customerService.deleteByID(id) ? ResponseEntity.ok().build() : ResponseEntity.notFound().build();
     }
 }
